98826460f36311f940bf2eb9ca15298fbc229840
夏珏琼 <jueqiongxia@infinite.net.cn>
2022-08-16 11:16:14 +0800

    完成第一版

--- README.md
+++ README.md
@@ -10,63 +10,51 @@ $ npm install xiajueqiong --save-dev
 $ yarn add xiajueqiong
 ```
 
-### 初始化SSO登录
+### 初始化参数
 
 在 main.js 中写入以下内容：
 
 ```js
-import { ssoLogin } from 'xiajueqiong'
+import Vue from 'vue'
+import { sso } from 'xiajueqiong'
 
 /**
  * @param {Object} option
  * @param {string} option.platform 产品编码(平台)
- * @param {string} option.baseURL 基础路径
- * @param {string} option.code 权限状态码
  * @param {string} option.env 环境
  */
 
-const option = { platform, baseURL, code, env }
-const instance = ssoLogin(option)
-
-export const createAPI = (url, method, params, config = {}) => {
-  if (method === 'get') {
-    config.params = params
-  } else {
-    config.data = params
-  }
-  return instance({
-    url,
-    method,
-    ...config
-  })
-}
-
-export default instance
+Vue.use(sso, { platform, env })
 ```
 
 | 参数名   | 必选 | 说明                                      | 类型   | 可选值        | 默认值 |
 | -------- | ---- | ----------------------------------------- | ------ | ------------- | ------ |
 | platform | 是   | 产品编码(平台) ，系统注册时的产品编码     | string | -             | -      |
-| baseURL  | 是   | api基础路径                               | string | -             | -      |
-| code     | 否   | 权限状态码                                | string | -             | 0001   |
 | env      | 是   | 环境（开发：dev，测试：test，生产：prod） | string | dev/test/prod | dev    |
 
-## SSO退出登录
+## SSO退出
 
 ```vue
-<div @click="logoutFn">退出</div>
-
-<script>
-  import { ssoLogout } from 'xiajueqiong'
-    
-  export default {
-    methods: {
-        logoutFn() {
-          ssoLogout()
-        }
-    }
-  }
-</script>
-
+<div @click="$ssoLogout()">退出</div>
 ```
 
+## SSO登录拦截
+
+在http.js文件中写入以下内容：
+
+```js
+import axios from 'axios'
+const instance = axios.create()
+
+/**
+ * @param {Object} option
+ * @param {Object} option.axiso 函数对象
+ * @param {string} option.code 权限状态码
+ */
+ssoLogin({ axiso:instance，code })
+```
+
+| 参数名 | 必选 | 说明           | 类型   | 可选值 | 默认值 |
+| ------ | ---- | -------------- | ------ | ------ | ------ |
+| axiso  | 是   | axios 函数对象 | Object | -      | -      |
+| code   | 是   | 权限状态码     | string | -      | -      |
--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
 import qs from 'qs'
-import axios from 'axios'
-
-const objectToFormdata = (obj, form, namespace) => {
-    let fd = form || new FormData()
-    let formKey
-
-    for (let property in obj) {
-        if (obj.hasOwnProperty(property)) {
-            let key = Array.isArray(obj)
-                ? `[${property}]`
-                : `.${property}`
-            if (namespace) {
-                formKey = namespace + key
-            } else {
-                formKey = property
-            }
-
-            if (obj[property] instanceof Date) {
-                fd.append(formKey, obj[property].toISOString())
-            } else if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-                objectToFormdata(obj[property], fd, formKey)
-            } else if (obj[property] !== undefined) {
-                fd.append(formKey, obj[property])
-            }
-        }
-    }
-
-    return fd
-}
 
 const ssoUrl = {
     dev: 'https://sso-test.ybj.com',
@@ -35,48 +6,33 @@ const ssoUrl = {
     prod: 'https://sso-new.ybj.com'
 }
 
-const ssoLogin = (config) => {
-    let _this = {
-        platform: '',
-        baseURL: '',
-        code: '0001',
-        env: 'dev'
-    }
-    Object.keys(config).forEach((key) => {
-        _this[key] = config[key]
-    })
-    // 参数挂载window
-    window.ssoParams = _this 
-
-    const instance = axios.create({
-        baseURL: _this.baseURL
-    })
-
-    instance
-        .interceptors
-        .request
-        .use(config => {
-            const token = localStorage.getItem('token')
-            token && (config.headers.common['token'] = token)
-            if (config.method === 'post' && config.data) {
-                if (config.headers['Content-Type'] === 'multipart/form-data') {
-                    config.data = objectToFormdata(config.data)
-                    config.headers.common['Content-Type'] = 'multipart/form-data'
-                }
-            }
-            return config
-        }, error => {
-            return Promise.reject(error)
+const sso = { // 数据初始化
+    platform: '',
+    env: 'dev',
+    install(Vue, options) {
+        Object.keys(options).forEach((key) => {
+            this[key] = options[key]
         })
-
-    instance.interceptors.response.use(
+        Vue.prototype.$ssoLogout = this.ssoLogout
+    },
+    ssoLogout() {
+        const params = {
+            type: 'logout',
+            platform: sso.platform,
+            redirectUrl: window.location.origin
+        }
+        window.location.replace(`${ssoUrl[sso.env]}?${qs.stringify(params)}`)
+    }
+}
+const ssoLogin = ({axiso, code}) =>{
+    axiso.interceptors.response.use(
         response => {
-            if (response.data.code === _this.code) {
+            if (response.data.code === code) {
                 const params = {
-                    platform: _this.platform,
+                    platform: sso.platform,
                     redirectUrl: window.location.href
                 }
-                window.location.replace(`${ssoUrl[_this.env]}?${qs.stringify(params)}`)
+                window.location.replace(`${ssoUrl[sso.env]}?${qs.stringify(params)}`)
             }
             return response
         },
@@ -84,16 +40,7 @@ const ssoLogin = (config) => {
             return Promise.reject(error)
         }
     )
-    return instance
+    return axiso
 }
 
-const ssoLogout = () => {
-    const params = {
-        type: 'logout',
-        platform: window.ssoParams.platform,
-        redirectUrl: window.location.origin
-    }
-    window.location.replace(`${ssoUrl[window.ssoParams.env]}?${qs.stringify(params)}`)
-}
-
-export { ssoLogin, ssoLogout }
+export { sso , ssoLogin }
