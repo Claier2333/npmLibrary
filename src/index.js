3590299343e276dfff67213c1358ef57d6342ad9
夏珏琼 <jueqiongxia@infinite.net.cn>
2022-08-16 18:31:14 +0800

    登录退出逻辑分离

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 import qs from 'qs'
-
 const ssoUrl = {
     dev: 'https://sso-test.ybj.com',
     test: 'https://sso-test.ybj.com',
     prod: 'https://sso-new.ybj.com'
 }
-
 const sso = { // 数据初始化
     platform: '',
-    env: 'dev',
-    install(Vue, options) {
-        Object.keys(options).forEach((key) => {
-            this[key] = options[key]
-        })
-        Vue.prototype.$ssoLogout = this.ssoLogout
-    },
-    ssoLogout() {
-        const params = {
-            type: 'logout',
-            platform: sso.platform,
-            redirectUrl: window.location.origin
-        }
-        window.location.replace(`${ssoUrl[sso.env]}?${qs.stringify(params)}`)
+    env: '',
+    install (Vue, options = {}) {
+        Object.assign(this, options)
     }
 }
-const ssoLogin = ({axiso, code}) =>{
+
+const useSSOLogout = () => {
+    const params = {
+        type: 'logout',
+        platform: sso.platform,
+        redirectUrl: window.location.origin
+    }
+    window.location.replace(`${ssoUrl[sso.env]}?${qs.stringify(params)}`)
+}
+
+const useSSOLogin = ({ axiso, code, status }) => {
     axiso.interceptors.response.use(
         response => {
-            if (response.data.code === code) {
+            if (response.data.code === code || response.status === status) {
                 const params = {
                     platform: sso.platform,
                     redirectUrl: window.location.href
@@ -43,4 +40,4 @@ const ssoLogin = ({axiso, code}) =>{
     return axiso
 }
 
-export { sso , ssoLogin }
+export { sso, useSSOLogin, useSSOLogout }
